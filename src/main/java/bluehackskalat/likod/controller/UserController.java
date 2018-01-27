866479a3c287d747f6c82958135499153c6f5a2e
Kyle-Althea <kyle-althea_santos@dlsu.edu.ph>
2018-01-27 20:09:32 +0800

    transfered donate to usercontroller

--- src/main/java/bluehackskalat/likod/controller/UserController.java
+++ src/main/java/bluehackskalat/likod/controller/UserController.java
@@ -6,6 +6,7 @@ import bluehackskalat.likod.modelo.beneficiary.Beneficiary;
 import bluehackskalat.likod.modelo.beneficiary.BeneficiaryManager;
 import bluehackskalat.likod.modelo.user.User;
 import bluehackskalat.likod.modelo.user.UserManager;
+import bluehackskalat.likod.transaksyon.DonationManager;
 
 public class UserController {
 	Gson gson = new Gson();
@@ -15,6 +16,30 @@ public class UserController {
 		String jsonInString = gson.toJson(user);
 		System.out.println("user json : " + jsonInString);
 		return jsonInString;
+	}
+	
+	public void donateUsingMoney(String uName, long amount, String bName) {
+		User u = UserManager.getInstance().getUser(uName);
+		Beneficiary b = BeneficiaryManager.getInstance().getBeneficiary(bName);
+		
+		if(DonationManager.donateUsingMoney(u, amount, b)) {
+			System.out.println("Successfully donated");
+		}
+		else {
+			System.out.println("Failed to donate");
+		}
+	}
+	
+	public void donateUsingCredit(String uName, long amount, String bName) {
+		User u = UserManager.getInstance().getUser(uName);
+		Beneficiary b = BeneficiaryManager.getInstance().getBeneficiary(bName);
+		
+		if(DonationManager.donateUsingCredit(u, amount, b)) {
+			System.out.println("Successfully donated");
+		}
+		else {
+			System.out.println("Failed to donate");
+		}
 	}
 
 }
